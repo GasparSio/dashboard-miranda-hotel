fe45df90eba8d53a1805aea1f67e616976cd05c4
Gaspar Sio <sio.gaspar@gmail.com>
2023-09-29 09:26:34 +0200

    new test on failed login

--- cypress/e2e/login.failed.cy.js
+++ cypress/e2e/login.failed.cy.js
@@ -1,15 +1,11 @@
-describe("Login", () => {
-    it("If login success then navigates to dashboard", () => {
-      cy.visit("localhost:3000");
-      cy.url().should("include", "/login");
-      cy.get('input[placeholder="Username"]')
-        .type("gas1")
-        .should("have.value", "gas");
-      cy.get('input[placeholder="Email"]')
-        .type("sio1")
-        .should("have.value", "sio");
-      cy.contains("Submit").click();
-  
-      cy.url().should("include", "/home/dashboard");
-    });
-  });
+describe('Login', () => {
+    it('It displays error message and stays on login when login fails', () => {
+        cy.visit("localhost:3000");
+        cy.url().should('include', '/login');
+        cy.get('input[placeholder="Username"]').type("wrongUsername");
+        cy.get('input[placeholder="Email"]').type("wrongEmail");
+        cy.contains("Submit").click();
+        cy.contains("Wrong Username or Email").should("be.visible");
+        cy.url().should("include", "login");
+    })
+})
